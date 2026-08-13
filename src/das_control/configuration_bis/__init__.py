f3d486505e7205f97dce35c1b4bbe26446e7333e
pymodeller <pymodeller@gmail.com>
2026-08-13 11:18:36 +0200

    fix: update templates

--- src/das_control/configuration_bis/__init__.py
+++
@@ -1,7 +0,0 @@
-"""AUTO-GENERATED MODELS PACKAGE."""
-
-from .general_settings import GeneralSettings
-from .process_settings import ProcessSettings
-from .zone import ZoneModel
-
-__all__ = ["GeneralSettings", "ProcessSettings", "ZoneModel"]
